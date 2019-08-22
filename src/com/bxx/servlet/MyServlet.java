34ed42ef75f5b3af5eee2f61ecdd02c7bd0b4243
Serica <943914044@qq.com>
2019-08-22 20:30:22 +0800

    first trial

--- src/com/bxx/servlet/MyServlet.java
+++ src/com/bxx/servlet/MyServlet.java
@@ -3,17 +3,27 @@ package com.bxx.servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("It is a GET request.");
+        System.out.println(req.getQueryString());
         super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("This is a POST request.");
+        System.out.println(req.getReader().readLine());
+        PrintWriter out = resp.getWriter();
+
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json; charset=utf-8");
+        out.append("{\"hi\": \"h\"}");
+        out.flush();
+        out.close();
         super.doPost(req, resp);
     }
 }
