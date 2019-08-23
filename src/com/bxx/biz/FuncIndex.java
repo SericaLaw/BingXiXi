41c69e9e6de83a1e668b311b6c4ac8b579e29718
SiriusXf <1508577104@qq.com>
2019-08-23 23:40:58 +0800

    The function handwriting of IndexServlet added

---
+++ src/com/bxx/biz/FuncIndex.java
@@ -0,0 +1,25 @@
+package com.bxx.biz;
+import java.util.ArrayList;
+import com.bxx.common.Message;
+public class FuncIndex {
+	public static ArrayList<Message>BrandProductPicture() //请求品牌商商品标题、分类、主图、状态
+	{
+		return null;
+	}
+	public static ArrayList<Message>BrandWaitingOrder()//请求品牌商所有待支付商品标题、价格、数量、sku、订单编号、订单创建时间
+	{
+		return null;
+	}
+	public static ArrayList<Message>BorrowProduct() //请求借卖方商品图片、价格、名称
+	{
+		return null;
+	}
+	public static ArrayList<Message>WishList() //请求借卖方心愿单图片、价格、sku
+	{
+		return null;
+	}
+	public static ArrayList<Message>BorrowWaitingOrder()//请求品牌商所有待支付商品标题、价格、数量、sku、订单编号、订单创建时间
+	{
+		return null;
+	}
+}
