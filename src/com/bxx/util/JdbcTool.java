b1f4230a2557244332fb4135f490b5716652ff8a
Serica <943914044@qq.com>
2019-08-23 22:09:25 +0800

    updae

--- src/com/bxx/util/JdbcTool.java
+++ src/com/bxx/util/JdbcTool.java
@@ -3,7 +3,7 @@ package com.bxx.util;
 import java.sql.*;
 
 public class JdbcTool {
-	static final String databasePath = "jdbc:Access:///C:/Users/Surface/Documents/GitHub/BingXiXi/db.accdb";
+	static final String databasePath = "jdbc:Access:///C:/Users/Serica/Workspace/BingXiXi/db.accdb";
 	public static Connection conn;
 
 	static {
@@ -16,9 +16,7 @@ public class JdbcTool {
 		}
 	}
 
-	/**
-	 * 执行sql 增删改 不包括查询
-	 */
+
 	public static boolean executeSql(String sql) {
 		try {
 			Statement st = (Statement) conn.createStatement();
@@ -31,9 +29,6 @@ public class JdbcTool {
 		}
 	}
 
-	/**
-	 * 执行sql 查询 返回结果为 ResultSet 结果集
-	 */
 	public static ResultSet executeSqlByQuery(String querysql) {
 		ResultSet rs = null;
 		try {
