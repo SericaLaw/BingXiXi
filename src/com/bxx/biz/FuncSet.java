f27d23037f71a0de3ff9f0d8277d79deb080ebbc
kxvrh <940546571@qq.com>
2019-08-24 10:42:05 +0800

    brandServlet completed

--- src/com/bxx/biz/FuncSet.java
+++ src/com/bxx/biz/FuncSet.java
@@ -11,26 +11,5 @@ public class FuncSet {
 		return op.insert(ew);
 	}
 
-	public static boolean addFunc(String chineseName, String englishName, String introduction, String type,
-			String url) {
-		// TODO Auto-generated method stub
-		System.out.println("此处需要数据库增操作");
-		//增加一个公司信息
-		return false;
-	}
 
-	public static boolean checkFunc(String email, String cash, String password) {
-		// TODO Auto-generated method stub
-		System.out.println("此处需要数据库查与改操作");
-		//提现，检查密码是否正确，提现的金额是否正确
-		return false;
-	}
-
-	public static ArrayList<BrandOrder> displayFunc(String email) {
-		// TODO Auto-generated method stub
-		System.out.println("此处需要数据库查操作");
-		//提现流水
-		ArrayList<BrandOrder> orders = new ArrayList<BrandOrder>();
-		return orders;
-	}
 }
